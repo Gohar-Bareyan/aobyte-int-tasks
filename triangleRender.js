44b6144c90f2607577b33b3103a842f3315ab886
Gohar-Bareyan <gohar.bareyan98@gmail.com>
2023-07-10 10:56:02 +0400

    Resolved the fixes

--- triangleRender.js
+++ triangleRender.js
@@ -1,36 +1,53 @@
 let isRotated = false;
+let input = document.getElementById('number');
+let renderButton = document.getElementById("renderButton");
+let rotateButton = document.getElementById("rotateButton");
+let triangleContainer = document.getElementById('triangle');
+let rotatedTriangleContainer = document.getElementById('rotatedTriangle');
+
+document.addEventListener("DOMContentLoaded", function () {
+    renderButton.addEventListener("click", render);
+    rotateButton.addEventListener("click", rotate);
+});
 
 function render() {
-    let inputNumber = document.getElementById('number');
-    let triangleContainer = document.getElementById('triangle');
-    let rotatedTriangleContainer = document.getElementById('rotatedTriangle');
-
-    let number = parseInt(inputNumber.value);
-
-    if (isNaN(number) || number <= 0) {
-        alert('Please enter a valid number greater than 0');
-    } else if (number > 30) {
-        alert("Please enter a number smaller than 30")
-        return
+    let number = parseInt(input.value);
+    if (!isValidNumber(number)) {
+        return;
     }
 
-    let triangle = '';
+    let triangle = generateTriangle(number);
 
+    attachTriangleToDOM(triangle);
+}
+
+function isValidNumber(number) {
+    if (isNaN(number) || number <= 1) {
+        alert('Please enter a valid number greater than 1');
+        return false;
+    } else if (number > 30) {
+        alert('Please enter a number smaller than 30');
+        return false;
+    }
+    return true;
+}
+
+function generateTriangle(number) {
+    let triangle = '';
     for (let i = 1; i <= number; i++) {
         let spaces = ' '.repeat(number - i);
-        let asterisks = '* '.repeat(i).trim();
+        let asterisks = '* '.repeat(i)
         triangle += spaces + asterisks + '\n';
     }
+    return triangle;
+}
 
+function attachTriangleToDOM(triangle) {
     triangleContainer.textContent = triangle;
     rotatedTriangleContainer.textContent = '';
 }
 
-
 function rotate() {
-    let triangleContainer = document.getElementById('triangle');
-    let rotatedTriangleContainer = document.getElementById('rotatedTriangle');
-
     if (!isRotated) {
         let triangleText = triangleContainer.textContent;
         let rotatedText = rotateText(triangleText);
